a49c825b2773e6e6b643025aba1b0737ccc5d132
rajgit2012 <rajnsu08@gmail.com>
2018-01-15 17:59:22 +0000

    Pushed schedulers

--- domain/src/main/java/com/mangolab/domain/usecase/UseCase.java
+++ domain/src/main/java/com/mangolab/domain/usecase/UseCase.java
@@ -1,9 +1,20 @@
 package com.mangolab.domain.usecase;
 
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.CompositeDisposable;
+
 /**
  * Created by rajforhad on 15/01/2018.
  */
 
 abstract class UseCase<T>{
+	private final CompositeDisposable compositeDisposable;
+	private final Scheduler backendExecutor;
+	private final Scheduler uiThreadExecutor;
 
+	protected UseCase(Scheduler backendExecutorScheduler, Scheduler uiThread) {
+		compositeDisposable = new CompositeDisposable();
+		backendExecutor = backendExecutorScheduler;
+		uiThreadExecutor = uiThread;
+	}
 }
